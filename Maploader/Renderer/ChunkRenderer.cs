d30b6fb7d1ed30b36f14cb81d1a2c82776f4f7a0
Markus Jungnickel <r@gwsa.de>
2019-05-07 00:13:12 +0200

    Useless stuff

--- Maploader/Renderer/ChunkRenderer.cs
+++ Maploader/Renderer/ChunkRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Linq;
 using JetBrains.Annotations;
 using Maploader.Extensions;
@@ -96,60 +95,6 @@ namespace Maploader.Renderer
                 g.DrawString($"{c.X * 16}, {c.Z * 16}", new Font(FontFamily.GenericSansSerif, 10), Brushes.Black,
                     xOffset, zOffset);
             }
-        }
-
-        public static bool IsOpaque(Image image)
-        {
-            var bitmap = new Bitmap(image);
-            var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly,
-                PixelFormat.Format32bppArgb);
-            unsafe
-            {
-                var p = (byte*) bitmapData.Scan0;
-                for (var x = 0; x < bitmap.Width; x++)
-                {
-                    for (var y = 0; y < bitmap.Height; y++)
-                    {
-                        if (p[x * 4 + y * bitmapData.Stride + 3] == 255) continue;
-                        bitmap.UnlockBits(bitmapData);
-                        return false;
-                    }
-                }
-            }
-
-            bitmap.UnlockBits(bitmapData);
-            return true;
-        }
-
-
-        private void AdjustBrightness(Graphics gr, Image image, float brightness)
-        {
-            // Make the ColorMatrix.
-            float b = brightness;
-            ColorMatrix cm = new ColorMatrix(new float[][]
-            {
-                new float[] {b, 0, 0, 0, 0},
-                new float[] {0, b, 0, 0, 0},
-                new float[] {0, 0, b, 0, 0},
-                new float[] {0, 0, 0, 1, 0},
-                new float[] {0, 0, 0, 0, 1},
-            });
-            ImageAttributes attributes = new ImageAttributes();
-            attributes.SetColorMatrix(cm);
-
-            // Draw the image onto the new bitmap while applying
-            // the new ColorMatrix.
-            Point[] points =
-            {
-                new Point(0, 0),
-                new Point(image.Width, 0),
-                new Point(0, image.Height),
-            };
-            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
-
-            // Make the result bitmap.
-            gr.DrawImage(image, points, rect,
-                GraphicsUnit.Pixel, attributes);
         }
     }
 }
